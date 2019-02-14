9e4458c467bedb806880d3a25b75a0c839419b7c
Gustavo Oliveira <gustavosantos510@yahoo.com.br>
2019-02-14 09:45:03 -0200

    Initial commit

---
+++ app/src/main/java/com/gustavo/mportal/makecoffeapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package com.gustavo.mportal.makecoffeapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView wb;
+    private WebSettings webSettings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        wb = (WebView) findViewById(R.id.webview);
+        wb.setWebChromeClient(new WebChromeClient());
+        webSettings = wb.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        wb.loadUrl("file:///android_asset/random_coffe.html");
+    }
+}
