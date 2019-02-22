24529d7722cbca73a075ae1eb487fb15ab7acf6f
Gustavo <gustavosantos510@yahoo.com.br>
2019-02-22 20:43:46 -0300

    Configuração Firebase + Clound Menssage

---
+++ app/src/main/java/com/gustavo/mportal/makecoffeapplication/service/FirebaseInstaceIDService.java
@@ -0,0 +1,8 @@
+package com.gustavo.mportal.makecoffeapplication.service;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class FirebaseInstaceIDService extends FirebaseInstanceIdService {
+
+
+}
---
+++ app/src/main/java/com/gustavo/mportal/makecoffeapplication/service/FirebaseService.java
@@ -0,0 +1,27 @@
+package com.gustavo.mportal.makecoffeapplication.service;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class FirebaseService extends FirebaseMessagingService {
+
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+    Log.i("FIREBASE SERVICE","Passei aqui");
+
+
+        super.onMessageReceived(remoteMessage);
+    }
+
+    @Override
+    public void onNewToken(String s) {
+
+        Log.i("FIREBASE SERVICE","Token: " + s);
+
+        super.onNewToken(s);
+    }
+}
